60e306afc4cc9e2ffc3108b67f6d667e53efde9f
Grace Wu <wjwu846@gmail.com>
2019-02-02 08:13:59 +1100

    refactoring to use player object

--- src/main/java/com/dius/interview/grace/Match.java
+++ src/main/java/com/dius/interview/grace/Match.java
@@ -2,5 +2,6 @@ package com.dius.interview.grace;
 
 public interface Match {
     public void pointWonBy(String player);
+
     public String score();
 }
--- src/main/java/com/dius/interview/grace/TennisMatch.java
+++ src/main/java/com/dius/interview/grace/TennisMatch.java
@@ -1,73 +1,91 @@
 package com.dius.interview.grace;
 
-public class TennisMatch implements Match{
+public class TennisMatch implements Match {
 
     private static final String[] SCORE_DESC = {"0", "15", "30", "40"};
 
-    private String player1Name;
-    private String player2Name;
-    private int player1Score;
-    private int player2Score;
-    private int player1GameScore;
-    private int player2GameScore;
+    private Player playerOne;
+    private Player playerTwo;
 
     public TennisMatch(String player1, String player2) {
-        player1Name = player1;
-        player2Name = player2;
-        player1Score = 0;
-        player2Score = 0;
-        player1GameScore = 0;
-        player2GameScore = 0;
+        this.playerOne = new Player(player1);
+        this.playerTwo = new Player(player2);
     }
 
     @Override
     public void pointWonBy(String player) {
-        if(player1Name.equals(player)) {
-            player1Score++;
-        }else {
-            player2Score++;
+        if (playerOne.getName().equals(player)) {
+            playerOne.winScore();
+        } else {
+            playerTwo.winScore();
         }
     }
 
     @Override
     public String score() {
 
-        // check if game is won by player
-        if(player1Score >= 4 || player2Score >= 4) {
-            int diff = player1Score - player2Score;
-            if(diff >= 2) {
-                player1GameScore++;
+        String gameScoreMsg = getGameScoreMsg();
+        ;
+
+        String scoreMsg = getScoreMsg();
+
+        return gameScoreMsg + scoreMsg;
+    }
+
+    private String getScoreMsg() {
+        String scoreMsg = "";
+        int playerOneScore = playerOne.getScore();
+        int playerTwoScore = playerTwo.getScore();
+        int scoreDiff = Math.abs(playerOneScore - playerTwoScore);
+
+        if (playerOneScore == 0 && playerTwoScore == 0) {
+            return scoreMsg;
+        } else {
+            scoreMsg = ", ";
+            if (playerOneScore >= 3 && playerTwoScore >= 3) {
+                // either deuce or advantage
+                if (scoreDiff == 0) {
+                    scoreMsg += "Deuce";
+                } else if (scoreDiff == 1) {
+                    scoreMsg += "Advantage " + getPlayerInLead().getName();
+                }
+            } else {
+                scoreMsg += SCORE_DESC[playerOneScore] + "-" + SCORE_DESC[playerTwoScore];
+            }
+        }
+
+        return scoreMsg;
+    }
+
+    private String getGameScoreMsg() {
+        int playerOneScore = playerOne.getScore();
+        int playerTwoScore = playerTwo.getScore();
+        int scoreDiff = Math.abs(playerOneScore - playerTwoScore);
+
+        // winning game only by leading 2 points
+        if (scoreDiff == 2) {
+            // at least scored 4 points
+            if (playerOneScore >= 4 || playerTwoScore >= 4) {
+                Player playerWonGame = getPlayerInLead();
+                playerWonGame.winGameScore();
                 resetGame();
-            }else if(diff <= -2) {
-                player2GameScore++;
             }
         }
 
-        String scoreMessage = player1GameScore + "-" + player2GameScore;
+        String gameScoreMsg = playerOne.getGameScore() + "-" + playerTwo.getGameScore();
 
-        if(player1Score >= 3 && player2Score >= 3) {
-            scoreMessage += ", ";
-            //either deuce or advantage
-            int scoreDiff = player1Score - player2Score;
-            if(scoreDiff == 0) {
-                scoreMessage += "Deuce";
-            }else if(scoreDiff == 1) {
-                scoreMessage += "Advantage " + getPlayerInLead();
-            }
-        }else if(player1Score != 0 && player2Score != 0){
-            scoreMessage += ", ";
-            scoreMessage += SCORE_DESC[player1Score] + "-" + SCORE_DESC[player2Score];
-        }
-
-        return scoreMessage;
+        return gameScoreMsg;
     }
 
     private void resetGame() {
-        player1Score = 0;
-        player2Score = 0;
+        playerOne.resetScore();
+        playerTwo.resetScore();
     }
 
-    private String getPlayerInLead() {
-        return player1Score > player2Score ? player1Name : player2Name;
+    private Player getPlayerInLead() {
+        if (playerOne.getScore() > playerTwo.getScore()) {
+            return playerOne;
+        }
+        return playerTwo;
     }
 }
