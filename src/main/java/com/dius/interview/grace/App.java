44a6508ddcc859fe00cce95a56678ea0279d31bb
Grace Wu <wjwu846@gmail.com>
2019-02-02 06:06:56 +1100

    init commit

---
+++ src/main/java/com/dius/interview/grace/App.java
@@ -0,0 +1,13 @@
+package com.dius.interview.grace;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
