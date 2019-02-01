6ba2412973a4eba90880a0a45d13f201c541b3ca
Grace Wu <wjwu846@gmail.com>
2019-02-02 06:21:25 +1100

    start with Match and test case

---
+++ src/main/java/com/dius/interview/grace/Match.java
@@ -0,0 +1,20 @@
+package com.dius.interview.grace;
+
+public class Match {
+
+    private String player1Name;
+    private String player2Name;
+
+    public Match(String player1, String player2) {
+        this.player1Name = player1;
+        this.player2Name = player2;
+    }
+
+    public void pointWonBy(String player) {
+
+    }
+
+    public String score() {
+        return "";
+    }
+}
---
+++ src/test/java/com/dius/interview/grace/MatchTest.java
@@ -0,0 +1,43 @@
+package com.dius.interview.grace;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MatchTest {
+
+    @Test
+    public void testWholeMatch() {
+        Match match = new Match("player 1", "player 2");
+
+//        match.pointWonBy("player 1");
+//        match.pointWonBy("player 2");
+//        assertThat("this will return \"0-0, 15-15\"",
+//                match.score(),
+//                is("0-0, 15-15"));
+//
+//        match.pointWonBy("player 1");
+//        match.pointWonBy("player 1");
+//        assertThat("this will return \"0-0, 40-15\"",
+//                match.score(),
+//                is("0-0, 40-15"));
+//
+//        match.pointWonBy("player 2");
+//        match.pointWonBy("player 2");
+//        assertThat("this will return \"0-0, Deuce\"",
+//                match.score(),
+//                is("0-0, Deuce"));
+//
+//        match.pointWonBy("player 1");
+//        assertThat("this will return \"0-0, Advantage player 1\"",
+//                match.score(),
+//                is("0-0, Advantage player 1"));
+//
+//        match.pointWonBy("player 1");
+//        assertThat("this will return \"1-0\"",
+//                match.score(),
+//                is("1-0"));
+    }
+
+}
